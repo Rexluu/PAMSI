076cb7a19c3ce39efd0efa6e93216f5c62a067bd
Rexluu <kjakub1234@gmail.com>
2017-03-12 15:59:18 +0100

    Changed names of functions

--- List_1/L1_Z3.cpp
+++ List_1/L1_Z3.cpp
@@ -3,23 +3,23 @@
 using namespace std; 
 
 
-long int silnia(int x)
+long int factorial(int x)
 {
 	if (x<0) cerr << "Nie mozna wyliczyc silnii z liczby ujemnej" << endl;
 	if (x<2) return 1;
 	
-	return x*silnia(x-1);
+	return x*factorial(x-1);
 }
 
-float potega(int x, int p)
+float exp(int x, int p)
 {
 	if (p==0) return 1;
 	
 	if (p%2!=0){
-		if (p>0) return x*potega(x,p-1);
-		if (p<0) return (1.0/x)*potega(x,p+1);
+		if (p>0) return x*exp(x,p-1);
+		if (p<0) return (1.0/x)*exp(x,p+1);
 	} else {
-		float tmp = potega(x,p/2);
+		float tmp = exp(x,p/2);
 		return tmp*tmp;
 	}
 }
@@ -32,7 +32,7 @@ int main()
 	cin >> x;
 	cout << "Podaj p: ";
 	cin >> p;
-	//cout << endl << endl << "n!  = " << silnia(x) << endl;
-	cout << "x^p = " << potega(x,p) << endl; 
+	cout << endl << endl << "n!  = " << factorial(x) << endl;
+	cout << "x^p = " << exp(x,p) << endl; 
 	return 0;
 }
